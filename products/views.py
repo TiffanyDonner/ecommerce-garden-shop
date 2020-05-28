5013f9cc2e490d6192373694b02205952a1ebb1a
Tiffany Donner <tiffany.donner@stsupport.se>
2020-05-28 08:07:09 +0000

    Updated views and urls. to display the product html page. Added products to installed in settings and added media paths.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
+def all_products(request):
+    """ Return and render all products """
+    products = Product.objects.all()
+    return render(request, "products.html", {"products": products})
