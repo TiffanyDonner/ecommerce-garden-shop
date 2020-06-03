fb254d81f9f2361a1f641d043a9aaf18c7baf605
Tiffany Donner <tiffany.donner@stsupport.se>
2020-06-03 19:40:41 +0000

    Added plant image for footer. Added in code for navbar in base.html,however gitpod is having issues. So I am not sure how it looks.

--- products/views.py
+++ products/views.py
@@ -12,22 +12,13 @@ def all_products(request):
 def product_category(request, category):
     """ Return and render all products """
     products = Product.objects.filter(category=category)
-    return render(request, "products.html", {"products": products})
+    categories = dict(Product.CATEGORY)
+    categories = categories.values()
+    return render(request, "products.html", {"products": products,
+                  'categories': categories})
 
 
 def product_detail(request, product_id):
     """ Return and render all products """
     product = Product.objects.get(id=product_id)
     return render(request, "product_detail.html", {"product": product})
-
-
-def view_categories(request, category):
-    """ Fetches all items belonging to a certain category. """
-
-    categories = dict(Product.CATEGORY)
-    categories = categories.values()
-
-    products = Product.objects.filter(category=category)
-    return render(request, 'products.html', {
-        'products': products,
-        'categories': categories})
