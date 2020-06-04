6b3131a4d176529d1b452d443515a38c0f668d89
Tiffany Donner <tiffany.donner@stsupport.se>
2020-06-04 23:00:13 +0000

    Added stripe payment and checkout app. Also formatted checkout.html. Edited views models and admi.py. Addeda urls.py file. All working

--- gardenshop/urls.py
+++ gardenshop/urls.py
@@ -19,6 +19,7 @@ from accounts import urls as accounts_urls
 from products import urls as urls_products
 from cart import urls as urls_cart
 from search import urls as urls_search
+from checkout import urls as urls_checkout
 from home import urls as urls_home
 from home.views import index
 from django.views import static
@@ -33,10 +34,10 @@ urlpatterns = [
     url(r'^accounts/', include(accounts_urls)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^search/', include(urls_search)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
 
 
 urlpatterns += staticfiles_urlpatterns()
-
