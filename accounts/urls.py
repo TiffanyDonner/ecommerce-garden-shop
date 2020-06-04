b74350667cf5a6a2e8ae46f45bda0ed250484d24
Tiffany Donner <tiffany.donner@stsupport.se>
2020-06-04 13:19:47 +0000

    Updated front end design for profile, registration and login to be more cohesive

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
-from accounts.views import logout, login, registration, user_profile, index
+from home.views import index
+from accounts.views import logout, login, registration, user_profile
 from accounts import url_reset
 
 urlpatterns = [
-    url(r'^$', index, name="account_index"),
+    url(r'^$', index, name="index"),
     url(r'^logout/', logout, name="logout"),
     url(r'^login/', login, name="login"),
     url(r'^register/', registration, name="registration"),
