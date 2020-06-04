9d5ea7e3ef410ddfd002a994eb55c941f6f6c37d
Tiffany Donner <tiffany.donner@stsupport.se>
2020-06-04 18:36:00 +0000

    Formatted products/urls.py file

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,8 @@ from .views import all_products, product_category, product_detail
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
-    url(r'^product/(?P<product_id>[0-9]+)', product_detail, name='product_detail'),
-    url(r'^category/(?P<category>[A-Za-z]+)', product_category, name="product_category"),
+    url(r'^product/(?P<product_id>[0-9]+)', product_detail,
+        name='product_detail'),
+    url(r'^category/(?P<category>[A-Za-z]+)', product_category,
+        name="product_category"),
 ]
