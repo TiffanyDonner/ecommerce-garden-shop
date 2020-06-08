66908706da3f84c3d0f539819da302e1443b2c97
Tiffany Donner <tiffany.donner@stsupport.se>
2020-06-08 02:35:35 +0000

    Updated url to change_password to password to link to the form in django.

--- accounts/urls.py
+++ accounts/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     url(r'^register/', registration, name="registration"),
     url(r'^profile/', user_profile, name="profile"),
     url(r'^edit/$', edit_profile, name="edit_profile"),
-    url(r'^change-password/', change_password, name="change_password"),
+    url(r'^password/', change_password, name="change_password"),
     url(r'^reset-password/', password_reset, name='reset_password'),
     url(r'^reset-password/done/', password_reset_done, name='password_reset_done'),
     url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/',
