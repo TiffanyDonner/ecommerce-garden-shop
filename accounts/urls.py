b30aab345d14c23c63bd397c94533482692b4a45
Tiffany Donner <tiffany.donner@stsupport.se>
2020-06-08 09:15:49 +0000

    Updated urls and urls_reset.py to link to password reset files

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,11 +7,11 @@ from accounts.views import (
     edit_profile,
     change_password,
 )
-
 from django.contrib.auth.views import (
     password_reset, password_reset_done, password_reset_confirm,
     password_reset_confirm, password_reset_complete,
-)    
+)
+from accounts import url_reset
 
 
 urlpatterns = [
@@ -21,10 +21,6 @@ urlpatterns = [
     url(r'^profile/', user_profile, name="profile"),
     url(r'^edit/$', edit_profile, name="edit_profile"),
     url(r'^password/', change_password, name="change_password"),
-    url(r'^reset-password/', password_reset, name='reset_password'),
-    url(r'^reset-password/done/', password_reset_done, name='password_reset_done'),
-    url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/',
-        password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset-password/complete/', password_reset_complete,
-        name='password_reset_complete'),
+    url(r'^password-reset/', include(url_reset))
+
 ]
