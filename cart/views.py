16fed6b0db7693593dd088cbf0e9516cd720f65d
Tiffany Donner <tiffany.donner@stsupport.se>
2020-06-08 02:52:43 +0000

    Updated cart.html to display go shopping if no items are in the cart

--- cart/views.py
+++ cart/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render, redirect, reverse
 # Create your views here.
 def view_cart(request):
     """A View that renders the cart contents page"""
-    return render(request, "cart.html")
+    context = {
+        'nothing_in_cart': True,
+    }
+    return render(request, "cart.html", context)
 
 
 def add_to_cart(request, id):
