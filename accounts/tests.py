9d7619a7d71d71cfd46b4399de6a6cd1848bd812
Tiffany Donner <tiffany.donner@stsupport.se>
2020-06-08 15:13:11 +0000

    Removed tests in tests.py in accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -4,13 +4,4 @@ from django.urls import reverse, resolve
 from django.contrib.auth import views as auth_views
 
 
-# Create your tests here.
-class TestUrls(SimpleTestCase):
 
-    def test_login_url_is_resolved(self):
-        url = reverse('login')
-        self.assertEqual(resolve(url).func.view_class, auth_views.LoginView)
-
-    def test_logout_url_is_resolved(self):
-        url = reverse('logout')
-        self.assertEqual(resolve(url).func.view_class, auth_views.LogoutView)
