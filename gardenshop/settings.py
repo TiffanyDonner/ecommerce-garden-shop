b16af4428f49fe175567dc42bd0dedc0a5a0ccb7
Tiffany Donner <tiffany.donner@stsupport.se>
2020-06-08 08:36:06 +0000

    Updated email host to gmail

--- gardenshop/settings.py
+++ gardenshop/settings.py
@@ -167,7 +167,7 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
